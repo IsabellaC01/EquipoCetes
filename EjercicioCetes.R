e50ff49a33d76eb571b4fbb3badda86fe7b3f921
IsabellaC01 <isabellacampos228@gmail.com>
2024-10-05 11:53:23 -0600

    Add files via upload

---
+++ EjercicioCetes.R
@@ -0,0 +1,11 @@
+# Se presentan las funciones de interés simple para CETES
+# Autor: Isabella Campos Ramírez
+
+# Valor futuro con Cetes:
+
+valorFinalSimple=function(VA,r,t){
+  Vf=VA*(1+(r*t))
+  
+  return(VF) 
+}
+ 
